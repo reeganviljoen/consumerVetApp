fc0a3aee9e8d0dce21746ea47bbdae024f2dbfb1
ReeganCodemaster <reeganviljoen@outlook.com>
2022-06-15 14:34:31 +0200

    added create method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,16 @@
 class UsersController < ApplicationController
+  def create
+    @response = HTTParty.post('http://localhost:3000/signup', 
+      body{
+        name: user_params[:user][:name], 
+        email: user_params[:user][:email], 
+        role: user_params[:user][:role], 
+        name: user_params[:user][:password]
+      })
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :role)
+  end
 end
