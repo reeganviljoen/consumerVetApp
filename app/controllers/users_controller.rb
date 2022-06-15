c612f24847ec4e950fde892bffc03ea1a8035238
ReeganCodemaster <reeganviljoen@outlook.com>
2022-06-15 14:45:35 +0200

    fixed params of new method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,10 @@
 class UsersController < ApplicationController
   def create
-    @response = HTTParty.post('http://localhost:3000/signup', 
-      body{
-        name: user_params[:user][:name], 
-        email: user_params[:user][:email], 
-        role: user_params[:user][:role], 
-        name: user_params[:user][:password]
-      })
+    @response = HTTParty.post('http://localhost:3000/signup', body{user_params})
   end
 
   private
   def user_params
-    params.require(:user).permit(:name, :email, :password, :role)
+    params.permit(:name, :email, :password, :role)
   end
 end
