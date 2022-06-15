fa48b90443bb1e3ceb61de554566f36cc2f1fa3e
ReeganCodemaster <reeganviljoen@outlook.com>
2022-06-15 16:48:45 +0200

    added create method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,12 @@
 class UsersController < ApplicationController
   def create
-    @response = HTTParty.post('http://localhost:3000/signup', body{user_params})
+    response = HTTParty.post('http://localhost:3000/signup', body:user_params)
+    binding.pry
+    if response.success?
+      redirect_to 
+    else 
+      render :new, status: :unprocessable_entity
+    end
   end
 
   private
