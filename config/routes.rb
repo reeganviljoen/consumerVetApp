92ad95c41ee1c4f84ea3278c3b812d74d7b72053
ReeganCodemaster <reeganviljoen@outlook.com>
2022-06-16 17:45:22 +0200

    fixed routes for signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get '/signup', to: 'users#new' 
-  post '/signin', to: 'users#create'
+  post '/users', to: 'users#create'
   # Defines the root path route ("/")
   # root "articles#index"
 end
