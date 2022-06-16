cdee5bc76174200df0d54b6ec531f872425ee0b2
ReeganCodemaster <reeganviljoen@outlook.com>
2022-06-16 18:02:05 +0200

    added signin routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get '/signup', to: 'users#new' 
   post '/users', to: 'users#create'
+
+  get '/signin', to: 'users#signin'
+  post '/sigin', to: 'users#authenticate'
   # Defines the root path route ("/")
   # root "articles#index"
 end
