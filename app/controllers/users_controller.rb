8481966f768e4d69a865dbe5f723dae5178d3280
ReeganCodemaster <reeganviljoen@outlook.com>
2022-06-17 09:38:18 +0200

    changed all response variables to instance varaibles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,23 +1,28 @@
 class UsersController < ApplicationController
   def create
-    response = HTTParty.post('http://localhost:3000/signup', body:user_params)
-    if response.success?
-      redirect_to "/users/#{response['id']}/show"
-      cookies[:token] = response['auth_token']
+    @response = HTTParty.post('http://localhost:3000/signup', body:user_params)
+    if @response.success?
+      redirect_to "/users/#{@response['id']}/show"
+      cookies[:token] = @response['auth_token']
     else 
       render :new, status: :unprocessable_entity
     end
   end
 
   def authenticate
-    response = HTTParty.post('http://localhost:3000/auth/login', body:user_params)
-    if response.success?
-      redirect_to "/users/#{response['id']}/show"
-      cookies[:token] = response['auth_token']
+    @response = HTTParty.post('http://localhost:3000/auth/login', body:user_params)
+    if @response.success?
+      redirect_to "/users/#{@response['id']}/show"
+      cookies[:token] = @response['auth_token']
     else 
       render :signin, status: :unauthorized
     end 
   end
+
+  def show
+    
+  end
+
   private
   def user_params
     params.permit(:name, :email, :password, :role)
