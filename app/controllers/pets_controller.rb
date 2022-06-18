a7a5e4fd9e7cadb596457f6b2c76483cdac1dc65
ReeganCodemaster <reeganviljoen@outlook.com>
2022-06-18 14:51:25 +0200

    added index method to petds controller

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,2 +1,12 @@
 class PetsController < ApplicationController
+  before_action :set_user
+  
+  def index
+    @pets = @user['pets']
+  end
+
+  private
+  def set_user
+    @user = JSON.parse(cookies[:user])
+  end
 end
