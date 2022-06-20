b0c072ed093fba60ca9b5e7d6ee4592958dcb584
ReeganCodemaster <reeganviljoen@outlook.com>
2022-06-20 09:30:31 +0200

    refactored cookie

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,13 @@ class UsersController < ApplicationController
   def create
     @response = HTTParty.post('http://localhost:3000/signup', body:user_params)
     if @response.success?
-      cookies[:user] = @response.to_s
-      cookies[:token] = @response['auth_token']
+      cookies[:user] = {
+        id:@response['id'],
+        email:@response['email'],
+        name:@response['name'],
+        pets:@response['pets'].length,
+        token:@response['auth_token']
+      }.to_json
       @user = JSON.parse(cookies[:user])
       redirect_to user_path(@user['id']) 
     else 
@@ -14,8 +19,14 @@ class UsersController < ApplicationController
   def authenticate
     @response = HTTParty.post('http://localhost:3000/auth/login', body:user_params)
     if @response.success?
-      cookies[:user] = @response.to_s
-      cookies[:token] = @response['auth_token']
+      cookies[:user] = {
+        id:@response['id'],
+        email:@response['email'],
+        name:@response['name'],
+        pets:@response['pets'].length,
+        token:@response['auth_token']
+      }.to_json
+
       @user = JSON.parse(cookies[:user])
       redirect_to user_path(@user['id']) 
     else 
