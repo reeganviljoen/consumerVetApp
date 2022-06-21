e1702bc9f99d7af6cfcb112c3b143130f9eff595
ReeganCodemaster <reeganviljoen@outlook.com>
2022-06-21 16:56:04 +0200

    added new and create mthods

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,2 +1,37 @@
 class RegistrationsController < ApplicationController
+  def new
+    header = {
+      'Authorization': cookies[:token]
+    }
+
+    user_response = HTTParty.get('http://localhost:3000/user', headers: header)
+    @user = JSON.parse(user_response.to_s)
+
+    vet_response = HTTParty.get('http://localhost:3000/vets', headers: header)
+    vets_hash = JSON.parse(vet_response.to_s)
+    @vets = []
+    vets_hash.each do |vet|
+      @vets.push([vet['name'],vet['email']])
+    end
+  end
+  
+  def create
+    header = {
+      'Authorization': JSON.parse(cookies[:user])['token']
+    }
+    response = HTTParty.post("http://localhost:3000/pets#{registration_params[:id]}/register", 
+                              body:{vet_email: registration_params[:vet_email]})
+    if @response.success?
+
+      @user = JSON.parse(cookies[:user])
+      redirect_to user_path(@user['id']) 
+    else 
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def registration_params
+    params.permit(:vet_email, :id)
+  end
 end
