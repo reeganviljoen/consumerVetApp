60b13a478ddbf6841c7f66d8d51691406f23ee2b
ReeganCodemaster <reeganviljoen@outlook.com>
2022-06-22 15:33:03 +0200

    added create method

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -17,6 +17,19 @@ class PetsController < ApplicationController
     @user = JSON.parse(user_response.to_s)
   end
 
+  def create
+    header = {
+      'Authorization': cookies['token']
+    }
+    response = HTTParty.post("http://localhost:3000/pets/create" , body: pet_params, headers:header)
+    if response.success?
+      @user = JSON.parse(response.to_s)
+      redirect_to user_path(@user['id'])  
+    else 
+      render :new, status: :unprocessable_entity, alert: 'Registraion unsucessfull'
+    end
+  end
+
   private
   def pet_params
     params.permit(:name, :animal)
