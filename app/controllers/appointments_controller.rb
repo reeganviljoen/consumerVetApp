4a662f2d3da4e9a726eea036bd1570cd47cc7da7
ReeganCodemaster <reeganviljoen@outlook.com>
2022-06-22 15:40:03 +0200

    added new method to controller

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,2 +1,11 @@
 class AppointmentsController < ApplicationController
+  def new
+    header = {
+      'Authorization': cookies[:token]
+    }
+    response = HTTParty.get('http://localhost:3000/user', headers: header)
+    @user = JSON.parse(response.to_s)
+  end
+
+  
 end
