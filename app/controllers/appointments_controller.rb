31146a76ec749a43918bdba3bcd95d7fee165b7a
ReeganCodemaster <reeganviljoen@outlook.com>
2022-06-22 17:28:50 +0200

    working progress: added create method

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -7,8 +7,38 @@ class AppointmentsController < ApplicationController
     @user = JSON.parse(response.to_s)
   end
 
+  def create 
+    header = {
+      'Authorization': cookies['token']
+    }
+
+    vet_response = HTTParty.get('http://localhost:3000/vets', headers: header)
+
+    vet_email = ''
+    vet_response.each do |vet|
+      vet['pets'].each do |pet|
+        pet['registrations'].each do |registration| 
+          if registration['id'] == appointment_params[:registration_id]
+            vet_email = vet['email']
+          end
+        end
+      end
+    end 
+
+    response = HTTParty.post("http://localhost:3000/pets/#{appointment_params[:pet_id]}/appointment" ,
+                              body: { vet_email: vet_email, date: appointment_params[:date] }, 
+                              headers: header)
+
+    if response.success?
+      @user = JSON.parse(response.to_s)
+      redirect_to user_path(@user['id'])  
+    else 
+      render :new, status: :unprocessable_entity
+    end
+  end
+
   private
-  def appoitment_params
-    params.permit(:date)
+  def appointment_params
+    params.permit(:date, :pet_id, :registration_id)
   end
 end
