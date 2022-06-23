d71715c2e123e9bd1ccb81dd50a32adffb315c56
ReeganCodemaster <reeganviljoen@outlook.com>
2022-06-23 14:31:36 +0200

    moved vet_mail into a helper method

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -11,26 +11,12 @@ class AppointmentsController < ApplicationController
     header = {
       'Authorization': cookies['token']
     }
-
-    vet_response = HTTParty.get('http://localhost:3000/vets', headers: header)
-    vet_email = ''
-    vet_response.parsed_response.each do |vet|
-      vet['pets'].each do |pet|
-        pet['registrations'].each do |registration| 
-          if registration['id'].to_s == appointment_params[:registration_id]
-            vet_email = vet['email']
-          end
-        end
-      end
-    end 
-
     response = HTTParty.post("http://localhost:3000/pets/#{appointment_params[:pet_id]}/appointment" ,
-                              body: { vet_email: vet_email, date: appointment_params[:date] }, 
+                              body: { vet_email: vet_email(header), date: appointment_params[:date] }, 
                               headers: header)
 
     if response.success?
       @user = response.parsed_response
-      binding.pry
       redirect_to user_path(@user['id'])  
     else 
       render :new, status: :unprocessable_entity
@@ -40,5 +26,20 @@ class AppointmentsController < ApplicationController
   private
   def appointment_params
     params.permit(:date, :pet_id, :registration_id)
+  end
+
+  def vet_email(header)
+    response = HTTParty.get('http://localhost:3000/vets', headers: header)
+    email = ''
+    response.parsed_response.each do |vet|
+      vet['pets'].each do |pet|
+        pet['registrations'].each do |registration| 
+          if registration['id'].to_s == appointment_params[:registration_id]
+            email = vet['email']
+          end
+        end
+      end
+    end 
+    email 
   end
 end
