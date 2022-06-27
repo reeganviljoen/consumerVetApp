eb684c547a8a658e8bea1d64717a94c15d70125f
ReeganCodemaster <reeganviljoen@outlook.com>
2022-06-27 10:54:30 +0200

    removed unnescarry coments

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
   get '/signup', to: 'users#new' 
   post '/users', to: 'users#create'
 
   get '/signin', to: 'users#signin'
   post '/signin', to: 'users#authenticate'
 
-  # get '/users/:id/show', to: 'users#show'
   resources :users, only: [:show] do
     resources :registrations, only: [:index, :update]
     resources :pets, only: [:index, :new, :create] do
@@ -16,6 +15,6 @@ Rails.application.routes.draw do
     end
   end
   get '/users/:id/pets', to: 'pets#index'
-  # Defines the root path route ("/")
+
   root "users#signin"
 end
