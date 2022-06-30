f76b2df98d19d627a58523b00b581b984ef0ccc7
ReeganCodemaster <reeganviljoen@outlook.com>
2022-06-30 14:25:52 +0200

    replaced create_appointment api call with method call

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -4,9 +4,7 @@ class AppointmentsController < ApplicationController
   end
 
   def create 
-    response = HTTParty.post("#{@api_url}/pets/#{appointment_params[:pet_id]}/appointment" ,
-                              body: { vet_email: vet_email, date: appointment_params[:date] }, 
-                              headers: @header)
+    response = VetsApi::AppoinntmentsApi.new(cookies[:token], params:appointment_params).create_appointment
     if response.created?
       @user = response.parsed_response
       redirect_to user_pets_path(@user['id'])  
@@ -22,20 +20,5 @@ class AppointmentsController < ApplicationController
   private
   def appointment_params
     params.permit(:date, :pet_id, :registration_id)
-  end
-
-  def vet_email
-    response = VetsApi::UsersApi.new(cookies[:token]).get_vets
-    email = ''
-    response.each do |vet|
-      vet['pets'].each do |pet|
-        pet['registrations'].each do |registration| 
-          if registration['id'].to_s == appointment_params[:registration_id]
-            email = vet['email']
-          end
-        end
-      end
-    end 
-    email 
   end
 end
--- app/vets_api/vets_api.rb
+++ app/vets_api/vets_api.rb
@@ -42,7 +42,28 @@ class VetsApi
   end
 
   class AppoinntmentsApi < VetsApi 
-    
+    def create_appointment
+      response = HTTParty.post("#{@api_url}/pets/#{@params[:pet_id]}/appointment" ,
+      body: { vet_email: vet_email, date: @params[:date] }, 
+      headers: @header)
+    end
+
+    private
+
+    def vet_email
+      response = HTTParty.get("#{@api_url}/vets", headers: @header)
+      email = ''
+      response.each do |vet|
+        vet['pets'].each do |pet|
+          pet['registrations'].each do |registration| 
+            if registration['id'].to_s == @params[:registration_id]
+              email = vet['email']
+            end
+          end
+        end
+      end 
+      email 
+    end
   end
 end
 
