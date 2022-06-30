bf0f270015ddd46b2afce3665d9d50c59e64a769
ReeganCodemaster <reeganviljoen@outlook.com>
2022-06-30 17:26:31 +0200

    changed all old classnames to new class name s

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -2,13 +2,13 @@ class PetsController < ApplicationController
   before_action :set_user, only: [:index, :new]
   
   def create
-    response = VetsApi::PetsApi.new(cookies[:token], params: pet_params).create_pet
+    response = VetsApi::Pet.new(cookies[:token], params: pet_params).create_pet
     if response.created?
       @user = response.parsed_response
       flash[:notice] = 'Created pet'
       redirect_to user_pets_path(@user['id'])  
     else 
-      @user = VetsApi::UsersApi.new(cookies[:token]).get_user
+      @user = VetsApi::User.new(cookies[:token]).get_user
       flash.now[:alert] = 'Couldnt create pet'
       render :new, status: :unprocessable_entity
     end
@@ -20,6 +20,6 @@ class PetsController < ApplicationController
   end
 
   def set_user
-    @user = VetsApi::UsersApi.new(cookies[:token]).get_user
+    @user = VetsApi::User.new(cookies[:token]).get_user
   end
 end
