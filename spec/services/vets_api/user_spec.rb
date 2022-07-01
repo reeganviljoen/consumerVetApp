53de2a06b112eed7c93a94acd64174fe66abf8cc
ReeganCodemaster <reeganviljoen@outlook.com>
2022-07-01 10:55:35 +0200

    added tests to test authenticate method

--- spec/services/vets_api/user_spec.rb
+++ spec/services/vets_api/user_spec.rb
@@ -21,14 +21,40 @@ RSpec.describe VetsApi::User do
     }
   end
 
-
   describe '#create_user' do
+    let(:user) do 
+      {
+        email: 'reegan@example.com',
+        name: 'reegan',
+        role: 'owner',
+        password:'password'
+      }.to_json
+    end
+
     it 'it invokes the api' do
       stub_request(:post, "#{url}/signup").
-        with(body: "name=reegan&email=reegan%40example.com&password=password&role=owner").
-        to_return(status: 200, body: body, headers: {})
+        with(body: user).
+          to_return(status: 200, body: body, headers: {})
 
       response = VetsApi::User.new(params: user).create_user
+      expect(response.body).to eq(body)
+    end
+  end
+
+  describe '#authenticate' do
+    let(:user) do 
+      {
+        email: 'reegan@example.com',
+        password:'password'
+      }
+    end
+    
+    it 'invokes the api' do  
+      stub_request(:post, "#{url}/auth/login").
+      with(body: user).
+        to_return(status: 200, body: body, headers: {})
+  
+      response = VetsApi::User.new(params: user).authenticate
       expect(response.body).to eq(body)
     end
   end
