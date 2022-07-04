2196198dd48186dc8a08e39e72e712ea518384c8
ReeganCodemaster <reeganviljoen@outlook.com>
2022-07-04 08:54:44 +0200

    added tests for post /users

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,10 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
-  # let(:url) { 'http://localhost' }
-  # stub_request(:post, url, '/signup')
+  describe "POST /users" do
+    let(:dummy_api) { instance_double 'VetsApi::User' }
+    let(:api_response) { instance_double HTTParty::Response, body: {id: 1}.to_json, code: 302}
 
-  # describe "POST /signup" do
-  #   before { }
-  # end
+    before do 
+      allow(VetsApi::User).to receive(:new).and_return(dummy_api)
+      allow(dummy_api).to receive(:create_user).and_return(api_response)
+      allow(api_response).to receive(:created?).and_return(true)
+      allow(api_response).to receive(:parsed_response).and_return({'id' => 1})
+      post '/users'
+    end
+    context 'when the login is valid' do
+      it 'redirects to users page' do
+        expect(response).to have_http_status(302)
+      end
+    end
+
+  end
 end
