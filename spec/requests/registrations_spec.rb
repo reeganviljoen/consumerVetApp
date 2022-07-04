256aff6d3fa73dc94819e483fd81fdfd539f1d22
ReeganCodemaster <reeganviljoen@outlook.com>
2022-07-04 16:20:53 +0200

    added tests for Post /users/:id/pets/:id/registrations

--- spec/requests/registrations_spec.rb
+++ spec/requests/registrations_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
-RSpec.describe "Registrations", type: :request do
-  describe "Get /users/:id/registrations" do
+RSpec.describe 'Registrations', type: :request do
+  describe 'Get /users/:id/registrations' do
     let(:dummy_api) { instance_double 'VetsApi::User' }
-    let(:api_response) { instance_double HTTParty::Response, body: {id: 1}.to_json}
+    let(:api_response) { instance_double HTTParty::Response}
     let(:user) do 
       {
         'id' => 1, 
@@ -40,7 +40,7 @@ RSpec.describe "Registrations", type: :request do
     end
   end
 
-  describe "Get /users/:id/pets/:id/registrations/new" do
+  describe 'Get /users/:id/pets/:id/registrations/new' do
     let(:dummy_api) { instance_double 'VetsApi::User' }
     let(:user_response) { instance_double HTTParty::Response}
     let(:user) do 
@@ -87,5 +87,23 @@ RSpec.describe "Registrations", type: :request do
     end
   end
 
+  describe 'Post /users/:id/pets/:id/registrations' do
+    let(:dummy_api) { instance_double 'VetsApi::Registration' }
+    let(:api_response) { instance_double HTTParty::Response}
+
+    before do
+      allow(VetsApi::Registration).to receive(:new).and_return(dummy_api)
+      allow(dummy_api).to receive(:create_registration).and_return(api_response)
+      allow(api_response).to receive(:parsed_response).and_return({'id' => 1})
+      allow(api_response).to receive(:created?).and_return(TRUE)
+      post user_pet_registrations_path(1,1)
+    end
+
+    context 'when the request is valid' do
+      it 'redirects properly' do
+        expect(response).to have_http_status(302)
+      end
+    end
+  end
 
 end
