9e3c32a3613deb5238f7e61065f1f4be5892148c
ReeganCodemaster <reeganviljoen@outlook.com>
2022-07-04 11:59:26 +0200

    added tests for POST /users/:id/pets

--- spec/requests/pets_spec.rb
+++ spec/requests/pets_spec.rb
@@ -1,7 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe "Pets", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe "POST /users/:id/pets" do
+    let(:dummy_api) { instance_double 'VetsApi::Pet' }
+    let(:api_response) { instance_double HTTParty::Response, body: {id: 1}.to_json, code: 302}
+
+    before do 
+      allow(VetsApi::Pet).to receive(:new).and_return(dummy_api)
+      allow(dummy_api).to receive(:create_pet).and_return(api_response)
+      allow(api_response).to receive(:created?).and_return(true)
+      allow(api_response).to receive(:parsed_response).and_return({'id' => 1})
+      post '/users/1/pets'
+    end
+
+    context 'when the signup is valid' do
+      it 'redirects to pets page succesfully' do
+        expect(response).to have_http_status(302)
+      end
+    end
   end
 end
