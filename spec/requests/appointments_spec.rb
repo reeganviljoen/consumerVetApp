e0b60850d512093dd60cd720887bfee0ed7b5765
ReeganCodemaster <reeganviljoen@outlook.com>
2022-07-04 17:08:45 +0200

    added tests for GET /appointment/:id

--- spec/requests/appointments_spec.rb
+++ spec/requests/appointments_spec.rb
@@ -1,7 +1,44 @@
 require 'rails_helper'
 
 RSpec.describe "Appointments", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe "GET /appointment/:id" do
+    let(:dummy_api) { instance_double 'VetsApi::User' }
+    let(:api_response) { instance_double HTTParty::Response}
+    let(:user) do 
+      {
+        'id' => 1, 
+        'pets' => 
+        [
+          {
+            'id' => 1,
+            'registrations' => 
+            [
+              {
+                'id' => 1,
+                'appointments' => 
+                [
+                  {
+                    'id' => 1,
+                    'date' => '2022-06-06'
+                  }
+                ]
+              }
+            ]
+          }
+        ]
+      }
+    end
+    before do
+      allow(VetsApi::User).to receive(:new).and_return(dummy_api)
+      allow(dummy_api).to receive(:get_user).and_return(api_response)
+      allow(api_response).to receive(:parsed_response).and_return(user)
+      get user_appointments_path(1)
+    end
+
+    context 'when the request is valid' do
+      it 'it retruns the page succesfully' do
+        expect(response).to have_http_status(200)
+      end
+    end
   end
 end
