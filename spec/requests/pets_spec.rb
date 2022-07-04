2706566c51a246ba3fa4114a65fce0f3eda9462d
ReeganCodemaster <reeganviljoen@outlook.com>
2022-07-04 14:02:53 +0200

    added tests for Get /users/:id/pets

--- spec/requests/pets_spec.rb
+++ spec/requests/pets_spec.rb
@@ -1,6 +1,45 @@
 require 'rails_helper'
 
 RSpec.describe "Pets", type: :request do
+  describe "Get /users/:id/pets" do
+    let(:dummy_api) { instance_double 'VetsApi::User' }
+    let(:api_response) { instance_double HTTParty::Response, body: {id: 1}.to_json}
+    let(:user) do 
+      {
+        'id' => 1, 
+        'pets' => 
+        [
+          {
+            'id' => 1,
+            'registrations' => 
+            [
+              {
+                'id' => 1,
+                'appointments' => 
+                [
+                  {'id' => 1}
+                ]
+              }
+            ]
+          }
+        ]
+      }
+    end
+    
+    before do 
+      allow(VetsApi::User).to receive(:new).and_return(dummy_api)
+      allow(dummy_api).to receive(:get_user).and_return(api_response)
+      allow(api_response).to receive(:parsed_response).and_return(user)
+      get '/users/1/pets'
+    end
+
+    context 'when the request is valid' do
+      it 'returns the page succesfully' do
+        expect(response).to have_http_status(200)
+      end
+    end
+  end
+
   describe "Get /users/:id/pets/new" do
     let(:dummy_api) { instance_double 'VetsApi::User' }
     let(:api_response) { instance_double HTTParty::Response, body: {id: 1}.to_json}
@@ -8,7 +47,7 @@ RSpec.describe "Pets", type: :request do
     before do 
       allow(VetsApi::User).to receive(:new).and_return(dummy_api)
       allow(dummy_api).to receive(:get_user).and_return(api_response)
-      allow(api_response).to receive(:parsed_response).and_return({'id' => 1}.to_json)
+      allow(api_response).to receive(:parsed_response).and_return({'id' => 1})
       get '/users/1/pets/new'
     end
 
