139e9c7200c606fa1e9ac61982524a9cd0f42bd5
ReeganCodemaster <reeganviljoen@outlook.com>
2022-07-04 12:00:03 +0200

    added tests for Get /users/:id/ew

--- spec/requests/pets_spec.rb
+++ spec/requests/pets_spec.rb
@@ -1,6 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe "Pets", type: :request do
+  describe "Get /users/:id/pets/new" do
+    let(:dummy_api) { instance_double 'VetsApi::User' }
+    let(:api_response) { instance_double HTTParty::Response, body: {id: 1}.to_json}
+
+    before do 
+      allow(VetsApi::User).to receive(:new).and_return(dummy_api)
+      allow(dummy_api).to receive(:get_user).and_return(api_response)
+      allow(api_response).to receive(:parsed_response).and_return({'id' => 1}.to_json)
+      get '/users/1/pets/new'
+    end
+
+    context 'when the request is valid' do
+      it 'returns the page succesfully' do
+        expect(response).to have_http_status(200)
+      end
+    end
+  end
+
   describe "POST /users/:id/pets" do
     let(:dummy_api) { instance_double 'VetsApi::Pet' }
     let(:api_response) { instance_double HTTParty::Response, body: {id: 1}.to_json, code: 302}
