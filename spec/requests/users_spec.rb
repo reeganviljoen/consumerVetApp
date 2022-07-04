76d264ad7548b0372d83ec69c7e4c7433359ee82
ReeganCodemaster <reeganviljoen@outlook.com>
2022-07-04 09:27:46 +0200

    added test fot /post login

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -12,11 +12,31 @@ RSpec.describe "Users", type: :request do
       allow(api_response).to receive(:parsed_response).and_return({'id' => 1})
       post '/users'
     end
-    context 'when the login is valid' do
+
+    context 'when the signup is valid' do
       it 'redirects to users page' do
         expect(response).to have_http_status(302)
       end
     end
+  end
 
+  describe 'Post /login' do
+    let(:dummy_api) { instance_double 'VetsApi::User' }
+    let(:api_response) { instance_double HTTParty::Response, body: {id: 1}.to_json, code: 200}
+
+    before do
+      allow(VetsApi::User).to receive(:new).and_return(dummy_api)
+      allow(dummy_api).to receive(:authenticate).and_return(api_response)
+      allow(api_response).to receive(:success?).and_return(true)
+      allow(api_response).to receive(:parsed_response).and_return({'id' => 1})
+      post '/signin'
+    end
+
+    context 'when the login is valid' do
+      it 'redirectss to the user page' do
+        binding.pry
+        expect(response).to have_http_status(302)
+      end
+    end
   end
 end
