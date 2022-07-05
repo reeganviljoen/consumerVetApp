aff9241710c635ce0038ed1319a9c21a9bd0008a
ReeganCodemaster <reeganviljoen@outlook.com>
2022-07-05 09:58:33 +0200

    refcatored tests

--- spec/requests/pets_spec.rb
+++ spec/requests/pets_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe "Pets", type: :request do
+  let(:api_response) { instance_double HTTParty::Response}
   describe "Get /users/:id/pets" do
     let(:dummy_api) { instance_double 'VetsApi::User' }
-    let(:api_response) { instance_double HTTParty::Response, body: {id: 1}.to_json}
     let(:user) do 
       {
         'id' => 1, 
@@ -33,7 +33,7 @@ RSpec.describe "Pets", type: :request do
       allow(VetsApi::User).to receive(:new).and_return(dummy_api)
       allow(dummy_api).to receive(:get_user).and_return(api_response)
       allow(api_response).to receive(:parsed_response).and_return(user)
-      get '/users/1/pets'
+      get user_pets_path(1)
     end
 
     context 'when the request is valid' do
@@ -45,13 +45,12 @@ RSpec.describe "Pets", type: :request do
 
   describe "Get /users/:id/pets/new" do
     let(:dummy_api) { instance_double 'VetsApi::User' }
-    let(:api_response) { instance_double HTTParty::Response, body: {id: 1}.to_json}
 
     before do 
       allow(VetsApi::User).to receive(:new).and_return(dummy_api)
       allow(dummy_api).to receive(:get_user).and_return(api_response)
       allow(api_response).to receive(:parsed_response).and_return({'id' => 1})
-      get '/users/1/pets/new'
+      get new_user_pet_path(1)
     end
 
     context 'when the request is valid' do
@@ -63,14 +62,13 @@ RSpec.describe "Pets", type: :request do
 
   describe "POST /users/:id/pets" do
     let(:dummy_api) { instance_double 'VetsApi::Pet' }
-    let(:api_response) { instance_double HTTParty::Response, body: {id: 1}.to_json, code: 302}
 
     before do 
       allow(VetsApi::Pet).to receive(:new).and_return(dummy_api)
       allow(dummy_api).to receive(:create_pet).and_return(api_response)
       allow(api_response).to receive(:created?).and_return(true)
       allow(api_response).to receive(:parsed_response).and_return({'id' => 1})
-      post '/users/1/pets'
+      post user_pets_path(1)
     end
 
     context 'when the request is valid' do
