0dbbf99c2ba3fe42a82c6f242afcba55bf410019
ReeganCodemaster <reeganviljoen@outlook.com>
2022-07-05 09:42:24 +0200

    aded tests for POST /users/:user_id/pets/:pet_id/registrations/:registration_id/appointments'

--- spec/requests/appointments_spec.rb
+++ spec/requests/appointments_spec.rb
@@ -1,33 +1,35 @@
 require 'rails_helper'
 
-RSpec.describe "Appointments", type: :request do
-  describe "GET users/:id/appointment" do
+RSpec.describe 'Appointments', type: :request do
+  let(:api_response) { instance_double HTTParty::Response}
+  let(:user) do 
+    {
+      'id' => 1, 
+      'pets' => 
+      [
+        {
+          'id' => 1,
+          'registrations' => 
+          [
+            {
+              'id' => 1,
+              'appointments' => 
+              [
+                {
+                  'id' => 1,
+                  'date' => '2022-06-06'
+                }
+              ]
+            }
+          ]
+        }
+      ]
+    }
+  end
+
+  describe 'GET users/:id/appointment' do
     let(:dummy_api) { instance_double 'VetsApi::User' }
-    let(:api_response) { instance_double HTTParty::Response}
-    let(:user) do 
-      {
-        'id' => 1, 
-        'pets' => 
-        [
-          {
-            'id' => 1,
-            'registrations' => 
-            [
-              {
-                'id' => 1,
-                'appointments' => 
-                [
-                  {
-                    'id' => 1,
-                    'date' => '2022-06-06'
-                  }
-                ]
-              }
-            ]
-          }
-        ]
-      }
-    end
+    
     before do
       allow(VetsApi::User).to receive(:new).and_return(dummy_api)
       allow(dummy_api).to receive(:get_user).and_return(api_response)
@@ -42,33 +44,9 @@ RSpec.describe "Appointments", type: :request do
     end
   end
 
-  describe "GET /users/:user_id/pets/:id/appointments/new" do
+  describe 'GET /users/:user_id/pets/:id/appointments/new' do
     let(:dummy_api) { instance_double 'VetsApi::User' }
-    let(:api_response) { instance_double HTTParty::Response}
-    let(:user) do 
-      {
-        'id' => 1, 
-        'pets' => 
-        [
-          {
-            'id' => 1,
-            'registrations' => 
-            [
-              {
-                'id' => 1,
-                'appointments' => 
-                [
-                  {
-                    'id' => 1,
-                    'date' => '2022-06-06'
-                  }
-                ]
-              }
-            ]
-          }
-        ]
-      }
-    end
+        
     before do
       allow(VetsApi::User).to receive(:new).and_return(dummy_api)
       allow(dummy_api).to receive(:get_user).and_return(api_response)
@@ -77,8 +55,26 @@ RSpec.describe "Appointments", type: :request do
     end
 
     context 'when the request is valid' do
-      it 'it retruns the page succesfully' do
+      it 'retruns the page succesfully' do
         expect(response).to have_http_status(200)
+      end
+    end
+  end
+
+  describe 'POST /users/:user_id/pets/:pet_id/registrations/:registration_id/appointments' do
+    let(:dummy_api) { instance_double 'VetsApi::Appointment' }
+
+    before do
+      allow(VetsApi::Appointment).to receive(:new).and_return(dummy_api)
+      allow(dummy_api).to receive(:create_appointment).and_return(api_response)
+      allow(api_response).to receive(:parsed_response).and_return(user)
+      allow(api_response).to receive(:created?).and_return(true)
+      post user_pet_registration_appointments_path(1,1,1)
+    end
+
+    context 'when the request is valid' do
+      it 'redirects the page sucefully' do
+        expect(response).to have_http_status(302)
       end
     end
   end
