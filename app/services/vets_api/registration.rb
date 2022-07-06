e8a32ad1b110d98de32ac9f444d96ed6e8bf8f04
ReeganCodemaster <reeganviljoen@outlook.com>
2022-07-06 10:32:52 +0200

    fixed api calls to refactored refactored api routes

--- app/services/vets_api/registration.rb
+++ app/services/vets_api/registration.rb
@@ -1,10 +1,10 @@
 class VetsApi::Registration < VetsApi
   def create_registration
-    response = HTTParty.post("#{@api_url}/pets/#{@params[:pet_id]}/register",
+    response = HTTParty.post("#{@api_url}/pets/#{@params[:pet_id]}/registrations",
     body: {vet_email: @params[:vet_email]}, headers: @header)
   end
 
   def accept_registration 
-    response = HTTParty.post("#{@api_url}/registrations/#{@params[:id]}", headers: @header)
+    response = HTTParty.put("#{@api_url}/registrations/#{@params[:id]}", headers: @header)
   end
 end
